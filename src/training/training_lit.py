fbca468e0a603ba022b4d0102b47aa3a830d7171
Ankita Makker <ankita@Shriramas-MacBook-Pro.local>
2021-01-04 13:42:19 +0530

    training script for lightning module

---
+++ src/training/training_lit.py
@@ -0,0 +1,35 @@
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.loggers import WandbLogger
+import wandb
+
+from FaceLandmarkDetection.src.detection.data.data_module import FaceLandMarkDataModule
+from FaceLandmarkDetection.config import *
+from FaceLandmarkDetection.src.training.lit_model import FaceLandMarkLitModel
+
+checkpoint_callback = ModelCheckpoint(
+    monitor='val_loss',
+    filepath=model_checkpoint,
+    save_top_k=3,
+    mode='min')
+
+params = dict(
+    epochs=num_epochs,
+    num_classes=num_classes,
+    batch_size=batch_size,
+    learning_rate=learning_rate,
+    dataset=dataset,
+    train_val_split_ratio=train_val_split_ratio,
+    architecture=architecture)
+
+wandb_logger = WandbLogger(project=project, config=params)
+
+with wandb.init(project=project, config=params):
+    # access all HPs through wandb.config, so logging matches execution!
+    dm = FaceLandMarkDataModule(data_dir)
+    trainer = pl.Trainer(max_epochs=wandb.config.epochs,
+                         logger=wandb_logger,
+                         progress_bar_refresh_rate=20,
+                         checkpoint_callback=checkpoint_callback)
+    model = FaceLandMarkLitModel(num_classes=wandb.config.num_classes)
+    trainer.fit(model, dm)
